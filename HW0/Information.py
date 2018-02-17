f3c4350a3f63458305ad9842354eb1bd4aa901ff
tnguyen452 <tnntech@gatech.edu>
2018-02-17 12:47:14 -0500

    Hang-man game

---
+++ HW0/Information.py
@@ -0,0 +1,3 @@
+# Hash table know as dictionary in python.
+
+# Array can be used contruct of hash table using integer. But if we want to store something other than integer such as string then we must use as dictionary.
