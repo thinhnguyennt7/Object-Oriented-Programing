f38ccdacdb2204c369547045555e329dc4b3d2fe
tnguyen452 <tnntech@gatech.edu>
2018-03-06 07:57:33 -0500

    update Fighter

--- HW4/Fighter.java
+++ HW4/Fighter.java
@@ -1,31 +1,61 @@
 /**
-*	Flighter class that implement all the method in Character
-*	abstract class. Extens class Character
-*
-*
+*Fighter class that implement all the method in Character
+*abstract class. Extens class Character
+*@author tnguyen452
+*@version 1.0
 */
 public class Fighter extends Character {
-	private int strength;
-	private Character c;
+    private int strength;
+    private Character c;
 
-	// Implement the constructor from the super class
-	public void attack(Character c) {
-		c.health = (c.health - 10) + strength;
-		if (c.isDead == true) {
-			System.out.println("Cannot attack a dead character");
-		}
-	}
+    /**
+    *The fighter constructor take in two parameter, corresponding with each of
+    *the constructors of the superclass
+    *@param name Take in the name of the fighter
+    *@param seed Take in the number of seed
+    */
+    public Fighter(String name, int seed) {
+        super(name, seed);
+    }
 
-	public void levelUp() {
-		c.level ++;
-		c.setHealth(5 * c.level);
-		c.strength += 2;
-		c.dexterity ++;
-		c.intelligence ++;
-		c.wisdom ++;
-	}
+    /**
+    *The Fighter constructor take in six parameters, corresponding with each of
+    *the constructors of the superclass
+    *@param name Take in the name of the fighter
+    *@param level Take in the level of the fighter
+    *@param strength Take in the strength value
+    *@param dexterity Take in the dexterity value
+    *@param intelligence Take in the intelligence value
+    *@param wisdom Take in the wisdom value
+    */
+    public Fighter(String name, int level, int strength, int dexterity,
+                    int intelligence, int wisdom) {
+            super(name, level, strength, dexterity, intelligence, wisdom);
+    }
 
-	public String toString() {
-		return "Level (" + c.level + ")" + "fighter named (" + c.name + ") with (" + c.strength + ") strength, (" + c.dexterity + ") dexterity, (" + c.intelligence + ") intelligence, and (" + c.wisdom + ") wisdom.";
-	}
+    @Override
+    public void attack(Character character) {
+        character.setHealth((character.getHealth() - 10) + strength);
+        if (character.getIsDead()) {
+            System.out.println("Cannot attack a dead character");
+        }
+    }
+
+    @Override
+    public void levelUp() {
+        c.setLevel(c.getLevel() + 1);
+        c.setHealth(5 * c.getLevel());
+        c.setStrength(c.getStrength() + 2);
+        c.setDexterity(c.getDexterity() + 1);
+        c.setIntelligence(c.getIntelligence() + 1);
+        c.setWisdom(c.getWisdom() + 1);
+    }
+
+    @Override
+    public String toString() {
+        return "Level (" + c.getLevel() + ")" + "fighter named (" + c.getName()
+                + ") with (" + c.getStrength() + ") strength, ("
+                + c.getDexterity() + ") dexterity, (" + c.getIntelligence()
+                + ") intelligence, and (" + c.getWisdom() + ") wisdom.";
+    }
 }
