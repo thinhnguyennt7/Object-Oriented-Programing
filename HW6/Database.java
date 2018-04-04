c45e3a0575d19a06a95cd2f273c996eb5983a84e
tnguyen452 <tnntech@gatech.edu>
2018-04-03 22:26:06 -0400

    Update

--- HW6/Database.java
+++ HW6/Database.java
@@ -1,3 +1,8 @@
+/**
+* A database of past Messages that have been sent
+*@author Tnguyen452
+*@version 1.0
+*/
 public class Database {
-	
+    
 }
---
+++ HW6/Message.java
@@ -0,0 +1,82 @@
+import java.ulti.Optional;
+import java.ulti.LocalDateTime;
+
+/**
+* A representation of a text message
+*@author Tnguyen452
+*@version 1.0
+*/
+public class Message {
+
+    private Optional<String> contactName;
+    private String to;
+    private String from;
+    private String body;
+    private LocalDateTime date;
+    private boolean isImportant;
+
+    /**
+    *
+    * Constructor for the message function take in several parameters
+    *@param contactName Take in the contact name of a person
+    *@param to Take in the person we want to send it TO
+    *@param from Take in the person we want to send it FROM
+    *@param body Take in the body content of the message
+    *@param date Take in the local date and time of the message
+    *@param isImportant Take in the condition that show if the message is
+    * important or not
+    *
+    */
+    public Message(Optional<String> contactName, String to, String from,
+        String body, LocalDateTime date, boolean isImportant) {
+        contactName = this.contactName;
+        to = this.to;
+        from = this.from;
+        body = this.body;
+        date = this.date;
+        isImportant = this.isImportant;
+    }
+
+   /**
+    * Getter method for the contact
+    */
+    public Optional<String> getContact() {
+        return contactName;
+    }
+
+    /**
+    * Getter method for the to the person want to send message to
+    */
+    public String getTo() {
+        return to;
+    }
+
+    /**
+    * Getter method for the from a person want to send the message
+    */
+    public String getFrom() {
+        return from;
+    }
+
+    /**
+    * Getter method for the body message
+    */
+    public String getBody() {
+        return body;
+    }
+
+    /**
+    * Getter method for the local date time
+    */
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    /**
+    * Getter method for the isImportant
+    */
+    public boolean getIsImportant() {
+        return isImportant;
+    }
+
+}
