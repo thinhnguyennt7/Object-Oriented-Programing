3b67833e0e01e8f22533cfae22028d7df4ee0c5e
tnguyen452 <tnntech@gatech.edu>
2018-04-03 23:10:51 -0400

    Update

--- HW6/Database.java
+++ HW6/Database.java
@@ -1,3 +1,7 @@
+import java.ulti.function.Predicate;
+import java.ulti.List;
+import java.ulti.Map;
+
 /**
 * A database of past Messages that have been sent
 *@author Tnguyen452
@@ -5,4 +9,44 @@
 */
 public class Database {
     
+    private List<Message> messages;
+
+    /**
+    *
+    * Constructor for the Data base function take in a parameter
+    * @param messages Take in the main messages
+    *
+    */
+    public Database(List<Message> messages) {
+        messages = this.messages;
+    }
+
+    public Message getMessage(int n) {
+        return n;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public List<Message> filter(Predicate<Message> filter) {
+        return 
+    }
+
+    public List<Message> getMessagesBetween(LocalDateTime start,
+        LocalDateTime end) {
+
+    }
+
+    public Map<String, List<Message>> sortMessagesByContact() {
+
+    }
+
+    public List<Message> getMessagesWithKeyword(String keyword) {
+
+    }
+
+    public List<Message> getMessagesWithPriority() {
+
+    }
 }
