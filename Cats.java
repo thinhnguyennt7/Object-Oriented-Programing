d7efdbfcf2dadf03a8cfe7822e71db17f434ccca
tnguyen452 <tnntech@gatech.edu>
2018-04-07 12:48:34 -0400

    Cats.java

---
+++ Cats.java
@@ -0,0 +1,15 @@
+/**
+* This is the Quiz Cats class
+*@author tnguyen452
+*@version 1.0
+*/
+public class Cats {
+	
+	/**
+	* The driver class
+	*@param String[] Take in the args
+	*/
+	public static void main(String[] args) {
+		
+	}
+}
