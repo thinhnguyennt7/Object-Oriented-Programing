8276d9ebd5f13817bc112bec6e5e9d19ecc420fc
tnguyen452 <tnntech@gatech.edu>
2018-04-16 11:57:01 -0400

    lamdba button

--- TodoList.java
+++ TodoList.java
@@ -1,32 +1,49 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
 
 public class TodoList extends Application {
 
 	@Override 
 	public void start(Stage stage) {
-		ListView<String> listView = new ListView<String>();
+
+		ObservableList<String> todos = FXCollections.observableArrayList();
+
+
+		ListView<String> listView = new ListView<String>(todos);
 
 		Button addButton = new Button("Add");
 
 		TextField inputField = new TextField();
 
+		// Button function
+		addButton.setOnAction(event -> {
+			todos.add(inputField.getText());
+			inputField.setText("");
+			inputField.requestFocus(); // We can countinue to type in the input file
+			System.out.println("Add new Todo");
+		});
+
 		HBox entryBox = new HBox();
 		entryBox.getChildren().addAll(inputField, addButton);
 
-		VBox vbox = new VBox();
+		VBox vbox = new VBox(); // As the root node
 		vbox.getChildren().addAll(listView, entryBox);
 
 		// Main windows
 		Scene scene = new Scene(vbox);
-		stage.setTitle("Todo");
+		stage.setTitle("Todos");
 		stage.setScene(scene);
 		stage.show();
 	}
+
 }
