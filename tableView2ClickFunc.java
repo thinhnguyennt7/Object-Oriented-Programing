95bf7eff8b43ca1b8bbda435187f8bbec4b63d0b
tnguyen452 <tnntech@gatech.edu>
2018-04-27 00:01:15 -0400

    new file

---
+++ tableView2ClickFunc.java
@@ -0,0 +1,21 @@
+                // } else if (event.getClickCount() == 2) {
+                //     if (isPlaying) {
+                //         mediaplayer.stop();
+                //         if (table.getSelectionModel().getSelectedItem()
+                //         != null)
+                //         {
+                //             Song selectedSong = table.getSelectionModel()
+                //                 .getSelectedItem();
+                //             theCurrentSong = (String) selectedSong
+                //                 .getFilename();
+                //             if (theCurrentSong != null) {
+                //                 musicFile = new Media(getClass().getResource(
+                //                     theCurrentSong).toExternalForm());
+                //                 mediaplayer = new MediaPlayer(musicFile);
+                //                 mediaplayer.play();
+                //                 clickPause.setDisable(false);
+                //                 isPlaying = true;
+                //             }
+                //         }
+                //     }
+                // }
